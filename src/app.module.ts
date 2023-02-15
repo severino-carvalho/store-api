8fda0a5a2eb42697acf23c68dbf092fa85b2118c
Severino Carvalho <severinocarvalho14@gmail.com>
2023-02-14 22:39:38 -0300

    CREATE: Database configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { DatabaseModule } from './database/database.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
