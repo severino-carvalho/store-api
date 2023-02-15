99975efe92886c9387d4d34f1b77962bae5bff79
Severino Carvalho <severinocarvalho14@gmail.com>
2023-02-15 19:50:50 -0300

    CREATE: Product resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './modules/user/user.module';
+import { ProductModule } from './modules/product/product.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
     DatabaseModule,
+    ProductModule,
   ],
 })
 export class AppModule {}
