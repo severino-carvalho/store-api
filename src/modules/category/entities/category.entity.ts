a7f2bc6fd2c385e055c0db573bda358e2084e29b
Severino Carvalho <severinocarvalho14@gmail.com>
2023-02-16 16:52:14 -0300

    UPDATE: Category Entity Implementation

--- src/modules/category/entities/category.entity.ts
+++ src/modules/category/entities/category.entity.ts
@@ -1,1 +1,23 @@
-export class Category {}
+import { Product } from 'src/modules/product/entities/product.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
+}
