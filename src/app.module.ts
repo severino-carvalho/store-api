b5a9f1634ec661dcc3d5b9b09397ed7c8f048b29
Severino Carvalho <severinocarvalho14@gmail.com>
2023-02-20 07:35:49 -0300

    CREATE: Resources of "Products" and "Categories"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, CategoriesModule],
 })
 export class AppModule {}
