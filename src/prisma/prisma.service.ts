b9cbfd0c3560903ac4fb549da64b0d4aff423872
Severino Carvalho <severinocarvalho14@gmail.com>
2023-02-21 11:14:57 -0300

    UPDATE: Adding loggin for development

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,55 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel>
+  implements OnModuleInit
+{
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'stdout',
+          level: 'query',
+        },
+        {
+          emit: 'stdout',
+          level: 'error',
+        },
+        {
+          emit: 'stdout',
+          level: 'info',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+      ],
+    });
+  }
+
+  private readonly logger = new Logger(PrismaService.name);
+
   async onModuleInit() {
+    this.$on('error', (event) => {
+      this.logger.error(event);
+    });
+    this.$on('warn', (event) => {
+      this.logger.warn(event);
+    });
+    this.$on('info', (event) => {
+      this.logger.verbose(event);
+    });
+    this.$on('query', (event) => {
+      this.logger.log(event);
+    });
+
     await this.$connect();
   }
 
