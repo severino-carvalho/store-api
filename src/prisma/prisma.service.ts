9427760a959da0ab42d2ead7936a0ca90ab4edf8
Severino Carvalho <severinocarvalho14@gmail.com>
2023-02-22 20:54:27 -0300

    UPDATE: Adjustment in prisma log

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -37,19 +37,6 @@ export class PrismaService
   private readonly logger = new Logger(PrismaService.name);
 
   async onModuleInit() {
-    this.$on('error', (event) => {
-      this.logger.error(event);
-    });
-    this.$on('warn', (event) => {
-      this.logger.warn(event);
-    });
-    this.$on('info', (event) => {
-      this.logger.verbose(event);
-    });
-    this.$on('query', (event) => {
-      this.logger.log(event);
-    });
-
     await this.$connect();
   }
 
