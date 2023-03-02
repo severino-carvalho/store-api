63e34d345961560ce2f3fc93a716dbe951f2b67a
Severino Carvalho <severinocarvalho14@gmail.com>
2023-03-02 16:42:29 -0300

    UPDATE: Implementing the add and remove cart method

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { ProductsService } from 'src/products/products.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CartService } from './cart.service';
+
 import { CartController } from './cart.controller';
+import { CartService } from './cart.service';
 
 @Module({
   controllers: [CartController],
-  providers: [CartService, PrismaService],
+  providers: [CartService, PrismaService, ProductsService],
 })
 export class CartModule {}
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,21 +1,75 @@
-import { Injectable } from '@nestjs/common';
-import { UpdateCartDto } from './dto/update-cart.dto';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { PrismaService } from 'src/prisma/prisma.service';
 import { ProductsService } from 'src/products/products.service';
+import { UpdateCartDto } from './dto/update-cart.dto';
 
 @Injectable()
 export class CartService {
-  constructor(private readonly prisma: PrismaClient) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly productService: ProductsService,
+  ) {}
 
   async findCart(user_id: number) {
-    return '';
+    const cart = await this.prisma.cart.findUnique({
+      where: { user_id },
+      include: { products: true },
+    });
+
+    if (!cart) throw new NotFoundException('User not found.');
+
+    return cart;
   }
 
-  async addOrRemoveToCart(user_id: number, updateCartDto: UpdateCartDto) {
-    return ``;
+  async addOrRemoveToCart(
+    user_id: number,
+    { product_id, quantity }: UpdateCartDto,
+  ) {
+    const cart = await this.ifProductExistsInCart(user_id, product_id);
+
+    if (cart) {
+      return await this.prisma.cart.update({
+        where: { user_id },
+        data: {
+          products: {
+            update: {
+              where: { product_id_cart_id: { cart_id: cart.id, product_id } },
+              data: { quantity },
+            },
+          },
+        },
+        include: { products: true },
+      });
+    }
+
+    if (!this.ifProductExists)
+      throw new NotFoundException('Product not exists.');
+
+    return await this.prisma.cart.update({
+      where: { user_id },
+      data: { products: { create: { product_id, quantity } } },
+      include: { products: true },
+    });
   }
 
   async deleteFromCart(user_id: number, updateCartDto: UpdateCartDto) {
     return ``;
+  }
+
+  private async ifProductExistsInCart(user_id: number, product_id: number) {
+    return await this.prisma.cart.findFirst({
+      where: {
+        user_id,
+        products: { some: { product_id } },
+      },
+      include: { products: true },
+    });
+  }
+
+  private async ifProductExists(product_id: number) {
+    const productExists = await this.productService.findOne(product_id);
+
+    return !!productExists;
   }
 }
