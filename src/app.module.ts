a328cd267dc15407c074142fc98346a51d33dd5b
Severino Carvalho <severinocarvalho14@gmail.com>
2023-03-03 20:17:37 -0300

    UPDATE: Resources user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CartModule } from './cart/cart.module';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule, CategoriesModule, CartModule, UserModule],
+  imports: [ProductsModule, CategoriesModule, CartModule, UsersModule],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {
-  constructor(private prisma: PrismaService) {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
 
   async findAll() {
-    return `This action returns all user`;
+    return `This action returns all users`;
   }
 
   async findOne(id: number) {
