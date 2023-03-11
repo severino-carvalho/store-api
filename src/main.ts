988b9669a6f599f978934350981bee97aa05f64d
Severino Carvalho <severinocarvalho14@gmail.com>
2023-03-11 00:18:01 -0300

    UPDATE: Swegger Configurations

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Store API')
+    .setDescription('The Store API')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.setGlobalPrefix('api');
 
