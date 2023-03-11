8f0fb29eceef3e242768c22d66f2ef9530a70b02
Severino Carvalho <severinocarvalho14@gmail.com>
2023-03-11 00:42:29 -0300

    UPDATE: Basic documentation of Users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @ApiProperty()
   name: string;
   @IsEmail()
+  @ApiProperty()
   email: string;
   @IsString()
+  @ApiProperty()
   password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,32 +10,39 @@ import {
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @ApiOperation({ summary: 'List all users.' })
   async findAll() {
     return await this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'List one user of id.' })
   async findOne(@Param('id') id: string) {
     return await this.usersService.findOne(+id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new user.' })
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.usersService.create(createUserDto);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update one user.' })
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return await this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Remove one user.' })
   async remove(@Param('id') id: string) {
     return await this.usersService.remove(+id);
   }
