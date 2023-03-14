484cd1fddf3d04fb8c6852acb67589c87324c86a
Severino Carvalho <severinocarvalho14@gmail.com>
2023-03-14 10:44:58 -0300

    UPDATE: Basic documentation of Product

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsArray,
   IsNotEmpty,
@@ -9,21 +10,26 @@ import {
 export class CreateProductDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty()
   price: number;
 
   @IsString()
   @IsOptional()
+  @ApiPropertyOptional()
   description?: string;
 
   @IsString()
   @IsOptional()
+  @ApiPropertyOptional()
   image?: string;
 
   @IsArray()
   @IsOptional()
+  @ApiPropertyOptional()
   categories?: number[];
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -10,12 +10,15 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('products')
+@ApiTags('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
+  @ApiOperation({ summary: 'List all products.' })
   findAll() {
     try {
       return this.productsService.findAll();
@@ -23,6 +26,7 @@ export class ProductsController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'List one product of id.' })
   findOne(@Param('id') id: string) {
     try {
       return this.productsService.findOne(+id);
@@ -30,6 +34,7 @@ export class ProductsController {
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new product.' })
   create(@Body() createProductDto: CreateProductDto) {
     try {
       return this.productsService.create(createProductDto);
@@ -37,6 +42,7 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update one product.' })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     try {
       return this.productsService.update(+id, updateProductDto);
@@ -44,6 +50,7 @@ export class ProductsController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete one product.' })
   remove(@Param('id') id: string) {
     try {
       return this.productsService.remove(+id);
