c8ab183c6dca4f8ae1bcf4e231e85ab583995293
Severino Carvalho <severinocarvalho14@gmail.com>
2023-03-14 11:51:05 -0300

    UPDATE: Sort by alpha

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,12 @@ async function bootstrap() {
     .setBasePath('/api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
