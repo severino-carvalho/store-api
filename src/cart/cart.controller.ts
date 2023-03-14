60c422c3da00a57f836da160b57c3e4e098e8204
Severino Carvalho <severinocarvalho14@gmail.com>
2023-03-14 11:50:43 -0300

    UPDATE: Basic documentation of Cart

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -2,17 +2,21 @@ import { Controller, Get, Body, Param, Delete, Put } from '@nestjs/common';
 
 import { CartService } from './cart.service';
 import { UpdateCartDto } from './dto/update-cart.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('carts')
+@ApiTags('carts')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find cart by id' })
   async findCart(@Param('id') user_id: string) {
     return await this.cartService.findCart(+user_id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update one product of cart by id' })
   async addOrRemoveToCart(
     @Param('id') user_id: string,
     @Body() updateCartDto: UpdateCartDto,
@@ -21,6 +25,7 @@ export class CartController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete one product of cart by id' })
   async deleteFromCart(
     @Param('id') user_id: string,
     @Body() updateCartDto: UpdateCartDto,
