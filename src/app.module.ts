012b88dc05ed33856931f83aec414eb9ec027305
Severino Carvalho <severinocarvalho14@gmail.com>
2023-03-17 00:26:43 -0300

    CREATE: Logic for authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CartModule } from './cart/cart.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductsModule, CategoriesModule, CartModule, UsersModule],
+  imports: [ProductsModule, CategoriesModule, CartModule, UsersModule, AuthModule],
 })
 export class AppModule {}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      privateKey: process.env.JWT_SECRETE_KEY,
+      signOptions: { expiresIn: '45s' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, PrismaService],
+})
+export class AuthModule {}
