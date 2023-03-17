014fbe295fb0270d0d4ab6da4f37bca75ee5c1cf
Severino Carvalho <severinocarvalho14@gmail.com>
2023-03-17 00:28:23 -0300

    UPDATE: Removing unnecessary imports

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ProductsService } from 'src/products/products.service';
