404101b220fef283653350c14e822919293f083a
Severino Carvalho <severinocarvalho14@gmail.com>
2023-03-18 10:09:35 -0300

    UPDATE: Security organization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
 import { CartModule } from './cart/cart.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
+import { AuthModule } from './security/auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
