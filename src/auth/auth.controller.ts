e15231d115f3aa72f4ce004938b7813c561a58b9
Severino Carvalho <severinocarvalho14@gmail.com>
2023-03-18 00:25:21 -0300

    UPDATE: Fixed bug in request data on documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import { AuthLoginDTO } from './dto';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(AuthGuard('local'))
   @Post('login')
+  @ApiBody({ type: AuthLoginDTO })
+  @ApiOperation({ summary: 'Route for login.' })
   async login(@Req() req: any) {
     return await this.authService.login(req.user);
   }
