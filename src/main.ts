83d0adcab3966ac06cfcedb53ab35afffd478006
Severino Carvalho <severinocarvalho14@gmail.com>
2023-03-20 22:43:26 -0300

    UPDATE: Import dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
